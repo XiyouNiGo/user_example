36ccb8bffb7ffd0bc92e070479132a67426e32c0
linkai.666 <linkai.666@bytedance.com>
2024-01-11 22:13:29 +0800

    feat: add glow for markdown

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   { import = "astrocommunity.pack.go" },
