788f84a11067e29198281723a881482f53b19caa
linkai.666 <linkai.666@bytedance.com>
2024-01-13 11:09:30 +0800

    feat: add lua language support

--- plugins/community.lua
+++ plugins/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
 
   { import = "astrocommunity.pack.helm" },
   { import = "astrocommunity.pack.docker" },
